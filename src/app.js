2a1b36490a39e711dd61ad95b81b9420c13f69e4
BrodyDubach <117411713+BrodyDubach@users.noreply.github.com>
2024-02-12 20:00:11 +0000

    Done.

--- src/app.js
+++ src/app.js
@@ -2,10 +2,41 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateExcuse = () => {
+  let who = ["My dog", "Our neighbor", "That lady", "My boss"];
+  let action = ["took my", "ate my", "cooked my", "drank my"];
+  let what = [
+    "first kiss",
+    "last chicken nugget",
+    "skateboard",
+    "only shirt",
+    "phone"
+  ];
+  let where = [
+    "at my new apartment",
+    "in a mcdonalds",
+    "in the center of the earth",
+    "from my front porch"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
